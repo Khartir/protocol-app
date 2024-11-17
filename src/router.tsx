d67fee40a4492763f4420a33594c0927ab3a7819
Thomas Gnandt <thomas.gnandt@mayflower.de>
2024-11-17 20:04:42 +0100

    add pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,34 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Settings } from "./Settings";
 import { Home } from "./Home";
 
 import { Targets } from "./Targets";
 import { Analytics } from "./Analytics";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/settings",
-        element: <Settings />,
-      },
-      {
-        path: "/target",
-        element: <Targets />,
-      },
-      {
-        path: "/analytics",
-        element: <Analytics />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/settings",
+          element: <Settings />,
+        },
+        {
+          path: "/target",
+          element: <Targets />,
+        },
+        {
+          path: "/analytics",
+          element: <Analytics />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
