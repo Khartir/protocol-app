ee9487d0e0aed6121a01c5f2e35d731e35030fd0
Thomas Gnandt <thomas.gnandt@mayflower.de>
2026-01-17 17:52:54 +0100

    improve test performance

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,12 +5,33 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: "happy-dom",
     setupFiles: ["./src/test/setup.ts"],
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
+    },
+    deps: {
+      optimizer: {
+        web: {
+          enabled: true,
+          include: [
+            "@mui/material",
+            "@mui/icons-material",
+            "@mui/x-charts",
+            "@mui/x-date-pickers",
+            "@emotion/react",
+            "@emotion/styled",
+            "rxdb",
+            "rxdb-hooks",
+            "recharts",
+            "formik",
+            "yup",
+            "dayjs",
+          ],
+        },
+      },
     },
   },
 });
